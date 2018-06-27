4c6746821d1a8045f9dbaa24d51f2621f31c0488
Paul Rosenberg <paulrosenberg13@gmail.com>
2018-06-26 21:32:32 -0700

    final changes made before submission

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 function enterMainContent() {
 	$(".button.enterHere").on("click", function(){
-		console.log("enterMainContent() ran");
 		$("#startPage").hide();
 		$("#mainContent").show();
 	});
@@ -10,7 +9,6 @@ function enterMainContent() {
 /* Modal functions */
 
 function handleModal() {
-	console.log("handleModal() ran");
 	let modal = $(".modal");
 	let btn = $("#modalBtn");
 	btn.on("click", function(){
@@ -27,8 +25,8 @@ function closeModal() {
 }
 
 
+
 function getDataFromWordsAPI(searchTerm) {
-	console.log("getDataFromWordsAPI() ran");
     let wordsUrlString = 'https://wordsapiv1.p.mashape.com/words/' + searchTerm;
 	$.ajax({
          url: wordsUrlString,
@@ -44,7 +42,6 @@ function getDataFromWordsAPI(searchTerm) {
 
 
 function getDataFromGiphyAPI(searchTerm, callback) {
-	console.log("getDataFromGiphyAPI() ran");
 	const giphyAPIkey = "E2G0ds6xGilsWofWW00ysSaG9QuWmfC3";
 	let settings = {
 		"async": true,
@@ -54,34 +51,31 @@ function getDataFromGiphyAPI(searchTerm, callback) {
 		"headers": {
 			"Cache-Control": "no-cache",
 		},
-		"success": renderGiphyImage,
-		"error": showGiphyError
+		"success": renderGiphyImage
 	}
 
 	$.ajax(settings);
 }
 
 
+
 function renderWordDetails(wordsApiData) {
-	console.log("renderWordDetails() ran");
 	let status = wordsApiData.success;
 	if (status == false) {
 		$("#searchTermResults").html('Ahhh - someone sprayed water in the server room.');	
 	} else if (!wordsApiData.results) {
-			console.log("no definition");
 			$("#wordDefinition").html("Can't find a definition for this one... try another word!")
 	} else {
 		let synonymsList = wordsApiData.results[0].synonyms;
 		if (synonymsList == undefined) {
-			// $("#wordItself").html(wordsApiData.word);
 			$("#wordDefinition").html(wordsApiData.results[0].definition);
 			$("#wordSynonyms").html("Doh. Looks like we're out of ideas on this one.");
 		} else {
-			// $("#wordItself").html(wordsApiData.word);
 			$("#wordDefinition").html(wordsApiData.results[0].definition);
 			$("#wordSynonyms").html(synonymsList.join(" "));
 				};
 	};
+	//These logic statements are letting the user know if there were problems finding data on the word they selected. If data was not found, messages are displayed.
 }
 		
 		
@@ -90,36 +84,21 @@ function renderWordDetails(wordsApiData) {
 
 
 function renderGiphyImage(giphyData){
-	// console.log(giphyData.meta.status);
-	// console.log(giphyData.meta.msg);
-	console.log("renderGiphyImage() ran")
-
 	let status = giphyData.meta.status;
 	let responseMessage = giphyData.meta.msg;
-	// let giphyUrlData = giphyData.data[0].images.original.url;
-
+	$("#giphyImage").show();
 	if (giphyData.pagination.total_count == 0) {
-		console.log("image undefined");
 		$("#giphyResult").html(`<img src="https://github.com/pauldoubleyouare/API_Capstone_TipOfTheTongue/blob/master/116456-200.png?raw=true">`);
 	} else $("#giphyImage").attr("src", `${giphyData.data[0].images.original.url}`);
 }
 
 function showWordError(jsonData) {
-	console.log("there was an error with WordsAPI");
-
-	// $("#wordItself").html(" ");
 	$("#wordDefinition").html("Hmm... are you sure that was a real word? (check the spelling and try again.)");
 	$("#wordSynonyms").html(" ");
 }
 
 
-function showGiphyError(err) {
-	console.log("there was a Giphy error");
-}
-
-
 function listenForSubmit() {
-	console.log("listenForSubmit() ran");
 	$("#searchWordForm").submit(function(event) {
 		event.preventDefault();
 		const searchTarget = $(event.currentTarget).find(".searchTermInput");
