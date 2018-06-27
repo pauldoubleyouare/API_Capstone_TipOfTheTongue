a6937e9b7aa4999e48e825b3a9d11a80faf03619
Paul Rosenberg <paulrosenberg13@gmail.com>
2018-06-26 19:06:25 -0700

    updated all

--- index.js
+++ index.js
@@ -73,11 +73,11 @@ function renderWordDetails(wordsApiData) {
 	} else {
 		let synonymsList = wordsApiData.results[0].synonyms;
 		if (synonymsList == undefined) {
-			$("#wordItself").html(wordsApiData.word);
+			// $("#wordItself").html(wordsApiData.word);
 			$("#wordDefinition").html(wordsApiData.results[0].definition);
 			$("#wordSynonyms").html("Doh. Looks like we're out of ideas on this one.");
 		} else {
-			$("#wordItself").html(wordsApiData.word);
+			// $("#wordItself").html(wordsApiData.word);
 			$("#wordDefinition").html(wordsApiData.results[0].definition);
 			$("#wordSynonyms").html(synonymsList.join(" "));
 				};
@@ -86,17 +86,13 @@ function renderWordDetails(wordsApiData) {
 		
 		
 
-// function separateSynonyms(synonym){
-// 	console.log(synonym);
-// 	// for(let)
-// 	let htmlSyn = "<ul><li>" + synonym.join("</li><li>") + "</li></ul>";
-// 	return htmlSyn;
-// }
+
 
 
 function renderGiphyImage(giphyData){
 	// console.log(giphyData.meta.status);
 	// console.log(giphyData.meta.msg);
+	console.log("renderGiphyImage() ran")
 
 	let status = giphyData.meta.status;
 	let responseMessage = giphyData.meta.msg;
@@ -105,21 +101,13 @@ function renderGiphyImage(giphyData){
 	if (giphyData.pagination.total_count == 0) {
 		console.log("image undefined");
 		$("#giphyResult").html(`<img src="https://github.com/pauldoubleyouare/API_Capstone_TipOfTheTongue/blob/master/116456-200.png?raw=true">`);
-	} else $("#giphyResult").html(`<img src="${giphyData.data[0].images.original.url}">`);
-
-	// if (status == 200) {
-	// 	$("#giphyResult").html(`<img src="${giphyUrlData}">`);
-	// } else if (status >= 400 && status <= 430) {
-	// 	console.log("there was an error with Giphy search");
-	// 	$("#giphyResult").html(`<img src="https://giphy.com/embed/9Y5BbDSkSTiY8">`)
-	// };
-// put in an image if there is no gif
+	} else $("#giphyImage").attr("src", `${giphyData.data[0].images.original.url}`);
 }
 
 function showWordError(jsonData) {
 	console.log("there was an error with WordsAPI");
 
-	$("#wordItself").html(" ");
+	// $("#wordItself").html(" ");
 	$("#wordDefinition").html("Hmm... are you sure that was a real word? (check the spelling and try again.)");
 	$("#wordSynonyms").html(" ");
 }
@@ -136,6 +124,7 @@ function listenForSubmit() {
 		event.preventDefault();
 		const searchTarget = $(event.currentTarget).find(".searchTermInput");
 		const searchTerm = searchTarget.val();
+		$("#wordItself").html(searchTerm);
 		searchTarget.val("");
 		getDataFromWordsAPI(searchTerm);
 		getDataFromGiphyAPI(searchTerm, renderGiphyImage);
